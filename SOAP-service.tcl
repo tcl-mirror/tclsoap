bfcec6c7ae1bb816c9d6144bec60ab8f90910227
pt111992 <pt111992>
2001-02-26 12:39:15 +0000

    Added some significant updates to the way requests are handled. Added GET support Generalized the POST support. Still need to add DOM parsing to the base64 method.

--- SOAP-service.tcl
+++ SOAP-service.tcl
@@ -1,32 +1,243 @@
-# soapserver - Copyright (C) 2001 Pat Thoyts <pat@zsplat.freeserve.co.uk>
+# SOAP-service.tcl - Copyright (C) 2001 Pat Thoyts <pat@zsplat.freeserve.co.uk>
 #
-# Provide a SOAP demo service. base64 encode a string.
+# Provide a SOAP demo service.
+#
+# This package provides a simple HTTP server that is useful for stand-alone
+# testing of HTTP requests (including SOAP requests). This is not meant
+# to be a production-quality web server.
+#
+# Replies to GET requests with the contents of a file in a subdirectory if
+# the requested file can be found. Some simple filename extension to MIME
+# content-type matching is performed.
+#
+# POST requests are passed to a handler function, currently only /soap/base64
+# is actually valid and this returns a fixed base64 encoded string.
+#
+# The toplevel procedures are `start', `stop' and `stats' which respectively
+# start or stop the service, or provide some statistics on the requests 
+# handled so far.
+#
+# -------------------------------------------------------------------------
+# This software is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the accompanying file `LICENSE'
+# for more details.
+# -------------------------------------------------------------------------
 
-package provide SOAP::Service 1.0
+package provide SOAP::Service 0.4
 
 package require Trf
 
+# -------------------------------------------------------------------------
+
 namespace eval SOAP::Service {
     variable version 1.0
-    variable rcs_version { $Id$ }
-    variable socket {}
-    variable default_port 8001
-}
-
-proc SOAP::Service::start { {port {}} } {
+    variable rcs_version { $Id: SOAP-service.tcl,v 1.1 2001/02/15 23:21:37 pat Exp pt111992 $ }
     variable socket
-    variable default_port
-
-    if { $port == {} } {
-	set port $default_port
+    variable port
+    variable stats
+    array set stats {
+        zsplat-Base64 0
+        error_404  0
+        error_500  0
+        fault     0
     }
-    set socket [socket -server [namespace current]::service $port]
+    namespace export start stop stats
 }
+
+# -------------------------------------------------------------------------
+
+proc SOAP::Service::start { {server_port {80}} } {
+    variable socket
+    variable port
+
+    if { [catch { set s $socket }] != 0 } {
+        set socket {}
+    }
+    if { $socket != {} } {
+        return -code error "SOAP service already running on socket $socket"
+    }
+
+    set port $server_port
+    set socket [socket -server [namespace current]::service $port]
+    puts "SOAP service started on port $port"
+    return $socket
+}
+
+# -------------------------------------------------------------------------
 
 proc SOAP::Service::stop {} {
     variable socket
     close $socket
+    set socket {}
 }
+
+# -------------------------------------------------------------------------
+
+proc SOAP::Service::stats {} {
+    variable stats
+    set count 0
+    foreach uri [array names stats] {
+        puts "$uri $stats($uri)"
+        incr count $stats($uri)
+    }
+    return $count
+}
+
+# -------------------------------------------------------------------------
+
+proc SOAP::Service::service {channel client_addr client_port} {
+
+    # read the request (if any)
+    set request {}
+    set line {1}
+    while { $line != {} && ! [eof $channel] } {
+	gets $channel line
+	lappend request $line
+    }
+
+    puts "[join $request \n]"
+
+    set http_request [split [lindex $request 0] ]
+    set http_action  [lindex $http_request 0]  ;# type of request
+    set http_url     [lindex $http_request 1]  ;# what URL requested
+    
+    switch -- $http_action {
+	GET {
+	    set reply [get $http_url]
+	}
+	POST {
+            set reply [post $http_url $request $channel]
+	}
+	default {
+	    set reply [error500]
+	}
+    }
+    
+    puts $channel "$reply"
+    flush $channel
+    close $channel
+}
+
+# -------------------------------------------------------------------------
+
+proc SOAP::Service::post { url headers channel} {
+    # Get the amount of data from the Content-Length header and read it.
+    set data {}
+    set length [lsearch -regexp $headers {^Content-Length:}]
+    if { $length != -1 } {
+        set length [split [lindex $headers $length] :]
+        set length [expr [lindex $length 1] + 0]
+    }
+
+    if { $length > 0 } {
+        set data [read $channel $length]
+    }
+
+    switch -- $url {
+        /soap/base64 {
+            set reply [base64_service $data]
+        }
+        default {
+            set reply [error404] 
+        }
+    }
+    return $reply
+}
+
+# -------------------------------------------------------------------------
+
+proc SOAP::Service::get { path } {
+    set path [eval file join [split $path {\\/}] ] ;# make it relative
+    if { [file exists $path] && [file readable $path] && [file isfile $path]} {
+	set body {}
+	set f [open $path "r"]
+	while { ! [eof $f] } {
+	    gets $f l
+	    lappend body $l
+	}
+	close $f
+	set body [join $body "\n"]
+	
+	set head [join [list \
+		"HTTP/1.1 200 OK" \
+		"Content-Type: [content_type $path]" \
+		"Content-Length: [string length $body]" ] "\n"]
+        set reply "${head}\n\n${body}"
+    } else {
+        set reply [error404]
+    }
+
+    return $reply
+}
+
+# -------------------------------------------------------------------------
+
+proc SOAP::Service::content_type { file } {
+    set ext [file extension $file]
+    switch -- $ext {
+	.htm { set type text/html }
+	.xml { set type text/xml }
+	.jpg { set type image/jpeg }
+	.tcl { set type application/x-tcl }
+	default { set type text/plain }
+    }
+    return $type
+}
+
+# -------------------------------------------------------------------------
+
+proc SOAP::Service::error404 {} {
+    variable stats
+    incr stats(error_404)
+    set body [join [list \
+            "<html><head><title>File not found</title></head>"\
+            "<body><h1>Error 404 File not found</h1><p>" \
+            "The requested file could not be found on this server." \
+            "</p></body></html>" \
+            ] "\n" ]
+    
+    set head [join [list \
+	    "HTTP/1.1 404 Error File not found" \
+	    "Content-Type: text/html" \
+	    "Content-Length: [string length $body]"] "\n"]
+    
+    return "${head}\n\n${body}"
+}
+
+# -------------------------------------------------------------------------
+
+proc SOAP::Service::error500 {} {
+    variable stats
+    incr stats(error_500)
+
+    set body [list \
+	    "Requests must be GET or POST." ]
+    set head [list \
+	    "HTTP/1.1 500 ERROR Invalid HTTP request type" \
+	    "Content-Type: text/html" \
+	    "Content-Length: [string length $body]" ]
+    return "[join $head \n]\n\n[join $body \n]"
+}
+
+# -------------------------------------------------------------------------
+
+proc SOAP::Service::base64_service { request } {
+    variable stats
+    incr stats(zsplat-Base64)
+    set doc [gen_reply]
+    set prebody [dom::DOMImplementation serialize $doc]
+    dom::DOMImplementation destroy $doc            ;# clean up
+    regsub {<!DOCTYPE[^>]*>\n} $prebody {} body    ;# hack
+    set head [join [list \
+            "HTTP/1.1 200 OK" \
+            "Content-Type: text/xml" \
+            "Content-Length: [string length $body]"\
+            "" ] "\n" ]
+    return "${head}\n${body}"
+}
+
+# -------------------------------------------------------------------------
 
 proc SOAP::Service::gen_reply {} {
     set doc [dom::DOMImplementation create]
@@ -49,22 +260,8 @@ proc SOAP::Service::gen_reply {} {
     
 }
 
-proc SOAP::Service::service {channel client_addr client_port} {
-    set data {1}
-    while { $data != {} && ! [eof $channel] } {
-	gets $channel data
-	puts "$data"
-    }
-    
-    set reply [[namespace current]::gen_reply]
-    set body [dom::DOMImplementation serialize $reply]
-    set head [join [list \
-	    "HTTP/1.1 200 OK" \
-	    "Content-Type: text/xml" \
-	    "Content-Length: [string length $body]"\
-	    "" ] "\n" ]
-    
-    puts $channel "${head}\n${body}"
-    flush $channel
-    close $channel
-}
+# -------------------------------------------------------------------------
+
+# Local variables:
+#   indent-tabs-mode: nil
+# End:
