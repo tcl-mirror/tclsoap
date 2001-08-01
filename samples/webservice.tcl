ac393ca40e21f2f26a4816ce6463642adae6a34f
Pat Thoyts <patthoyts@users.sourceforge.net>
2001-08-01 23:45:44 +0000

    Sample service.

---
+++ samples/webservice.tcl
@@ -0,0 +1,58 @@
+# webservice.tcl - Copyright (C) 2001 Pat Thoyts <Pat.Thoyts@bigfoot.com>
+#
+# Client side to the TclSOAP public webservice SOAP interface.
+#
+# Usage: register a SOAPAction (equates to a namespace and file)
+#        upload a tclscript using 'save' to provide the implementation for you
+#        SOAPaction.
+#        Call your methods via cgi-bin/webservices.  The action registed will
+#        define what file is sourced into the Tcl interpreter to satisfy your
+#        requests.
+#        If you need to recover the Tcl script, use 'read'
+#        If you have had enough, use 'unregister' (when I write it!)
+#
+# -------------------------------------------------------------------------
+# This software is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the accompanying file `LICENSE'
+# for more details.
+# -------------------------------------------------------------------------
+#
+# @(#)$Id$
+
+namespace eval webservices {
+
+    variable uri    urn:tclsoap:webservices
+    variable action urn:tclsoap:webservices
+    variable proxy  http://tclsoap.sourceforge.net/cgi-bin/rpc
+
+    SOAP::create register \
+	    -uri $uri \
+	    -proxy $proxy \
+	    -action $action \
+	    -params {email string passwd string action string}
+    
+    SOAP::create save \
+	    -uri $uri \
+	    -proxy $proxy \
+	    -action $action \
+	    -params {email string passwd string action string filedata string}
+
+    SOAP::create read \
+	    -uri $uri \
+	    -proxy $proxy \
+	    -action $action \
+	    -params {email string passwd string action string}
+
+    SOAP::create unregister \
+	    -uri $uri \
+	    -proxy $proxy \
+	    -action $action \
+	    -params {email string passwd string action}
+}
+
+# -------------------------------------------------------------------------
+#
+# Local variables:
+# mode: tcl
+# End:
